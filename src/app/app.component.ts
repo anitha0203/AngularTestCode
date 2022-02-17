87c23e419a08013abd6aefd78680b3207dfea860
anitha0203 <anithab@visheshcountrycache.com>
2022-02-17 12:21:35 +0530

    AngularTestCode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  zipCode:number
+  data:any;
+  zcode: number;
+  url = 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=5a4b2d457ecbef9eb2a71e480b947604';
+  ccond: number;      temp: number;      temp_max: number;      temp_min: number;
+      
+    constructor(private http: HttpClient) {}
+
+    setZip(zipcode) {
+        this.zcode = zipcode;
+        localStorage.setItem('ZipCode : ', zipcode);
+        this.getData();
+    }
+    getData() {
+        this.http.get(this.url).subscribe((res) => {
+          this.data = res;
+          this.ccond = this.data.weather[0].main;
+          this.temp = this.data.main.temp;
+          this.temp_max = this.data.main.temp_max;
+          this.temp_min = this.data.main.temp_min;
+          console.log(this.ccond + '   ' + this.temp  + '   ' +this.temp_min + '   ' +this.temp_max );        
+    });    
+  }
+
+  sub(){
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent, ForecastComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
